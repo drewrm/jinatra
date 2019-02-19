65819856f9241a2293d2557e2b4a0b869653759f
Andrew Myers <andrew.myers@qantas.com.au>
2019-02-19 18:38:33 +1100

    Update Commons Lang Version

--- src/main/java/au/id/andrewmyers/jinatra/http/RouteParser.java
+++ src/main/java/au/id/andrewmyers/jinatra/http/RouteParser.java
@@ -7,7 +7,7 @@ package au.id.andrewmyers.jinatra.http;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
