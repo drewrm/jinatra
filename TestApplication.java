de2d164c944860c451f34373678c709d50d793a1
Andrew Myers <drewrm@users.noreply.github.com>
2026-06-13 21:34:31 +1000

    Fix up warnings

--- TestApplication.java
+++ TestApplication.java
@@ -10,6 +10,33 @@ import au.id.andrewmyers.jinatra.annotations.*;
 @Application(name="Hello World", port=8090)
 public class TestApplication {
 
+    @Get(route = "/")
+    public void index(Request request, Response response, Callback callback) {
+        response.setStatus(200);
+        response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/html; charset=UTF-8");
+        Content.Sink.write(response, true, """
+                <html>
+                    <head>
+                        <title>Available Endpoints</title>
+                    </head>
+                    <body>
+                        <h1>Available Endpoints</h1>
+                        <ul>
+                            <li><a href="/">Index</a></li>
+                            <li><a href="/hello">Hello</a></li>
+                            <li><a href="/teapot">Tea Pot</a></li>
+                        </ul>
+                    </body>
+                </html>""", callback);
+    }
+
+    @Get(route="/teapot")
+    public void teapot(Request request, Response response, Callback callback) {
+        response.setStatus(418);
+        response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/html; charset=UTF-8");
+        Content.Sink.write(response, true, "<html><head><title>HTTP 418 - I am a teapot</title></head><body><h1>This is a tea pot</h1></body></html>", callback);
+    }
+
     @Get(route="/hello")
     public void hello(Request request, Response response, Callback callback) {
         response.setStatus(200);
--- src/main/java/au/id/andrewmyers/jinatra/http/HttpMethod.java
+++ src/main/java/au/id/andrewmyers/jinatra/http/HttpMethod.java
@@ -9,16 +9,16 @@ import java.lang.annotation.Annotation;
  */
 public class HttpMethod<T extends Annotation> {
 
-    public static final HttpMethod DELETE = new HttpMethod(Delete.class, "DELETE");
-    public static final HttpMethod GET = new HttpMethod(Get.class, "GET");
-    public static final HttpMethod OPTIONS = new HttpMethod(Options.class, "OPTIONS");
-    public static final HttpMethod PATCH = new HttpMethod(Patch.class, "PATCH");
-    public static final HttpMethod POST = new HttpMethod(Post.class, "POST");
-    public static final HttpMethod PUT = new HttpMethod(Put.class, "PUT");
+    public static final HttpMethod<Delete> DELETE = new HttpMethod<Delete>(Delete.class, "DELETE");
+    public static final HttpMethod<Get> GET = new HttpMethod<Get>(Get.class, "GET");
+    public static final HttpMethod<Options> OPTIONS = new HttpMethod<Options>(Options.class, "OPTIONS");
+    public static final HttpMethod<Patch> PATCH = new HttpMethod<Patch>(Patch.class, "PATCH");
+    public static final HttpMethod<Post> POST = new HttpMethod<Post>(Post.class, "POST");
+    public static final HttpMethod<Put> PUT = new HttpMethod<Put>(Put.class, "PUT");
 
     private Class<T> type;
     private String name;
-    private static final HttpMethod[] values = new HttpMethod[]{DELETE, GET, OPTIONS, PATCH, POST, PUT};
+    private static final HttpMethod<?>[] values = new HttpMethod<?>[]{DELETE, GET, OPTIONS, PATCH, POST, PUT};
 
     private HttpMethod(final Class<T> type, final String name) {
         this.type = type;
@@ -33,7 +33,7 @@ public class HttpMethod<T extends Annotation> {
         return name;
     }
     
-    public static HttpMethod[] values() {
+    public static HttpMethod<?>[] values() {
         return values;
     }
 }
